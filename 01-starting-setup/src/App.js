c2b61e873fda2b9a93af29bd67b6be51fb277776
prajjwal2411 <prajjwal1997@gmail.com>
2023-04-26 22:57:11 +0530

    react-udemy-tutorial-day3

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,8 +1,10 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <p>This is also visible here</p>
+      <ExpenseItem />
     </div>
   );
 }
