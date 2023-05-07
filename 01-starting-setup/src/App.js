cc39e3ca26f2e368aa0874e2969d69dd63bea6c5
prajjwal2411 <prajjwal1997@gmail.com>
2023-05-07 17:50:58 +0530

    react-udemy-tutorial-day7

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,6 +1,7 @@
-import Expenses from "./components/Expenses";
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
-export default function App() {
+const App = () => {
   const expenses = [
     {
       id: "e1",
@@ -30,8 +31,10 @@ export default function App() {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses items={expenses}/>
     </div>
   );
 }
+
+export default App;
